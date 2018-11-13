8000ac8f9e435b8197f99c198ff0c8187fc7ce8e
Dom Vinyard <domvinyard@me.com>
2018-11-13 12:13:34 +0000

    demo

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import Example from '../lib';
-import { SecondExample } from '../lib';
+import React from "react";
+import Rememberer, { useRemember } from "../lib";
 
-const App = () => (
-  <div>
-    <Example />
-    <SecondExample />
-  </div>
-);
+const Number = () => {
+  const [remember, the] = useRemember();
+  return <p>{the.number || 0}</p>;
+};
+
+const Button = () => {
+  const [remember, the] = useRemember();
+  return (
+    <button onClick={() => remember({ number: Math.random() })}>regen</button>
+  );
+};
+
+const App = () => {
+  return (
+    <Rememberer>
+      <h1>number</h1>
+      <Number />
+      <Number />
+      <Button />
+    </Rememberer>
+  );
+};
 
 export default App;
