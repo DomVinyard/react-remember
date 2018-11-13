2c5a0fcbc955ade3a28dc6d53dea684fa446b670
Dom Vinyard <domvinyard@me.com>
2018-11-13 10:54:33 +0000

    split demo

--- src/index.js
+++ src/index.js
@@ -1,29 +1,70 @@
-//Remember
-import React from "react";
-import ReactDOM from "react-dom";
-import Rememberer, { useRemember } from "./react-remember";
+import React, { useContext } from "react";
+import local from "local-storage";
+import ObjectInspector from "react-object-inspector";
 
-const Number = () => {
-  const [remember, the] = useRemember();
-  return <p>{the.number || 0}</p>;
+const Context = React.createContext();
+class Rememberer extends React.Component {
+  state = {};
+
+  componentDidUpdate() {
+    if (this.noPersist) return local.remove("state");
+    local.set("state", this.state);
+  }
+
+  componentDidMount() {
+    this.setState(local.get("state"));
+  }
+
+  clearState() {
+    local.remove("state");
+    window.location.reload();
+  }
+
+  renderTree() {
+    if (!Object.keys(this.state).length)
+      return "nothing remembered (link to docs)";
+    return (
+      <div>
+        <div style={{ marginBottom: "1rem" }}>
+          <span style={{ fontSize: "1.2rem" }}>Remembered</span>
+          <span style={{ float: "right" }}>
+            <button onClick={this.clearState.bind(this)}> 💣 Delete All</button>
+          </span>
+        </div>
+        <ObjectInspector
+          initialExpandedPaths={["root"]}
+          name="the"
+          data={this.state}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    const { children, show, noPersist } = this.props;
+    if (noPersist && !this.clearedForNoPersist) {
+      this.clearedForNoPersist = true;
+      local.remove("state");
+    }
+    if (show) return this.renderTree();
+    return (
+      <Context.Provider
+        value={{
+          useRemember: [
+            val => !this.noPersist && this.setState(val),
+            this.state
+          ]
+        }}
+      >
+        {children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Rememberer;
+
+export const useRemember = function Consumer() {
+  const { useRemember } = useContext(Context);
+  return useRemember;
 };
-
-const Button = () => {
-  const [remember, the] = useRemember();
-  return (
-    <button onClick={() => remember({ number: Math.random() })}>regen</button>
-  );
-};
-
-const App = () => {
-  return (
-    <Rememberer>
-      <h1>number</h1>
-      <Number />
-      <Number />
-      <Button />
-    </Rememberer>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById("root")); // 100
